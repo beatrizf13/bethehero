53d91f1cd5982faa37a7bd626af804ca71529451
Beatriz Felix <azz.felix7@gmail.com>
2020-03-27 23:47:51 -0300

    👷 build(backend): preparing for tests

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const database = require('../database/connection');
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
 
   async store(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await database('ongs').insert({
       id,
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,10 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(
-  process.env.NODE_ENV == 'development'
-    ? configuration.development
-    : configuration.production
-);
+const connection = knex(configuration[process.env.NODE_ENV]);
 
 module.exports = connection;
